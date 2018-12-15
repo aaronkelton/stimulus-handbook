4ac638a42055df0334971696f12c83b5c2a548ac
Aaron Kelton <aaron.k.kelton@gmail.com>
2018-12-15 11:31:14 -0600

    managing state

---
+++ src/controllers/slideshow_controller.js
@@ -0,0 +1,33 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+  static targets = [ "slide" ]
+
+  initialize() {
+    //const index = parseInt(this.element.getAttribute("data-slideshow-index"))
+    // const index = parseInt(this.data.get("index"))
+    this.showCurrentSlide()
+  }
+
+  next() {
+    this.index++
+  }
+
+  previous() {
+    this.index--
+  }
+
+  showCurrentSlide() {
+    this.slideTargets.forEach((el, i) => {
+      el.classList.toggle("slide--current", this.index == i)
+    })
+  }
+  get index() {
+    return parseInt(this.data.get("index"))
+  }
+
+  set index(value) {
+    this.data.set("index", value)
+    this.showCurrentSlide()
+  }
+}
